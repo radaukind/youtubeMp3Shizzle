d7ed0a8007ac43b256aec51e64d1c3105551471a
Arne Jütte <arne_juette@posteo.com>
2023-12-29 17:31:54 +0100

    search py extended

--- search.py
+++ search.py
@@ -2,5 +2,10 @@
  
 with open("bookmarks.html") as f:
     data = f.read()
-     
+    tagindex = data.find("test")
+
+    tagindexLower = tagindex -50
+    tagindexHigher = tagindex +50
+
 print(data)
+print(data[tagindexLower:tagindexHigher])
